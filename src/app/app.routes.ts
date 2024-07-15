16696223c61978632d507e84cb8843cac5aa7ca9
Aldo Souza <aldocnsouza@gmail.com>
2024-07-15 20:24:47 -0300

    config: Environment configuration

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { ChatbotComponent } from './pages/chatbot/chatbot.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+      path: "",
+      component: HomeComponent
+    },
+    {
+      path: "chat",
+      component: ChatbotComponent
+    },
+    {
+      path: "**",
+      pathMatch: "full",
+      redirectTo: ""
+    }
+];
